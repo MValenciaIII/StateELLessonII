d0555f77bb16f79d0514ff4f53379442f07987ab
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2024-02-08 14:39:20 -0600

    pushing

--- src/components/ComplexState.js
+++ src/components/ComplexState.js
@@ -1,12 +1,30 @@
 import React from 'react'
 
+function ComplexObjectFavorite(props) {
+    console.log(props)
+    //first way to utilize Ternary V
+    let favorite = props.colored ? "favoriteTrue" : "favoriteFalse";
+    //Second way 
+    //  <div onClick="" className={`text-center ${props.colored ? "favoriteTrue" : "favoriteFalse"}`}>x</div>
+
+
+    return(
+            <div>
+                {/* TERNARY operator can be utilized inside classNames as well!
+                DOn't forget about the Template Literals to be able to write other classNames inside
+                */}
+                <div onClick={props.handleClick} className={`text-center ${favorite}`}>x</div>
+            </div>
+    )
+}
+
 function ComplexObject() {
     const [contact, setContact] = React.useState({
         firstName: "John",
         lastName: "Doe",
         phone: "+1 (555) 555-5555",
         email: "jDoe420@yahoo.com",
-        isFavorite: false
+        isFavorite: true
     })
 
 
@@ -31,10 +49,12 @@ function ComplexObject() {
             <div className="row">
                 <div className="col-sm-12">
                     <h3>{contact.firstName} {contact.lastName}</h3>
-                    {/* TERNARY operator can be utilized inside classNames as well!
-                        DOn't forget about the Template Literals to be able to write other classNames inside
+                    {/* contact.isFavorite passing boolean(TRUE/FALSE) value 
+                        colored is going to be the props name.
+                        handleClick={} is the way we pass functions (ie.toggleFavorite) to child components
+                        and if needing to pass FUNCTIONS to them name props attribute "handleClick"
                     */}
-                    <div onClick={toggleFavorite} className={`text-center ${contact.isFavorite ? "favoriteTrue" : "favoriteFalse"}`}>x</div>
+                   <ComplexObjectFavorite colored={contact.isFavorite} handleClick={toggleFavorite} />
                     <p>{contact.phone}</p>
                     <p>{contact.email}</p>
                 </div>
--- src/components/CounterPractice.js
+++ src/components/CounterPractice.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
+function NumCounter(props) {
+    console.log(props)
+    return(
+        <div className="col-sm-4">
+            <h3>Current Number:{props.number}</h3>
+        </div>
+    )
+}
+
 function CounterPractice() {
     const [count, setCount] = React.useState(0)
-
+//  const [0, f()] = React.useState(0)
     function add() {
 
         //First way to pass new Values to useState.
@@ -33,9 +42,9 @@ function CounterPractice() {
             <div className="col-sm-4">
                 <input onClick={add} type="button" value="Add" />
             </div>
-            <div className="col-sm-4">
-                <h3>Current Number:{count}</h3>
-            </div>
+            {/* variable count is the State Value */}
+            {/* Whatever Attribute is named in this Component, We'll use this name to display props in this component */}
+            <NumCounter number={count} />
             <div className="col-sm-4">
                 <input onClick={subtract} type="button" value="Subtract" />
             </div>
