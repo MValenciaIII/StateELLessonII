da379f7da59d960279bede0c90589591a53721ba
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2024-02-14 13:20:33 -0600

    Forms

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 function Forms() {
+    const [formDataAll, setFormDataAll] = React.useState([])
     const [formData, setFormData] = React.useState({
         firstName:"",
         lastName:"",
-        email:""
+        email:"",
+        comments:"",
+        isFriendly: false,
+        employment: "",
+        favColor: ""
     })
     
 
@@ -15,12 +20,17 @@ function Forms() {
         //ie. <input First Name text box />
         console.log(event.target.name)
 
+
+        //REACT PRACTICE USUALLY DESTRUCTS OBJECTS and sets it as a VARIABLE
+        //ie. v
+        const {name, value, type, checked} = event.target
+
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
                 //ES6 Computed Properties
                 //Whatever current HTML element is calling handleEvent() the "event.target.name" gives us the name Value from that HTML element. 
-                [event.target.name] : event.target.value
+                [name] : type === 'checkbox' ? checked : value
             }
         })
 
@@ -45,6 +55,13 @@ function Forms() {
         // }
     }
 
+    function handleSubmit(event) {
+        //It PREVENTS our page from REFRESHING and re-rendering the page
+        event.preventDefault()
+
+        console.log(formData)
+    }
+
 
     return(
         <div className="row">
@@ -52,12 +69,61 @@ function Forms() {
                 <h1>Forms</h1>
             </div>
             <div className="col-sm-6">
-                <form action="">
+                {/* form has a attribute that handles submitting. onSubmit={RUNFUNCTIONs} */}
+                <form onSubmit={handleSubmit}>
+
+
                     {/* onChange = Anytime there is a change in the value or typed in this element. handleEvent Function will be ran */}
                     <input type="text" placeholder="First Name" name="firstName" id="" onChange={handleEvent} value={formData.firstName} />
-                    {/* name attribute allows us to uniquely identify current Element */}
+
+
+                    {/* name attribute allows us to uniquely identify current Element 
+                        MAKE SURE name attribute "VALUE" is the same exact as target STATE variable
+                    */}
                     <input type="text" placeholder='Last Name' name="lastName" onChange={handleEvent} value={formData.lastName}/>
-                    <input type="text" placeholder="Email" name="email" onChange={handleEvent} id="" />
+
+
+                    {/* Make sure to set value to STATE variable  */}
+                    <input type="text" placeholder="Email" name="email" onChange={handleEvent} id="" value={formData.email} />
+
+                    <textarea name="comments" placeholder='Comments' onChange={handleEvent} value={formData.comments} id="" cols="30" rows="10"></textarea>
+
+                    <input type='checkbox' id='isFriendly' name='isFriendly' value={formData.isFriendly} checked={formData.isFriendly} onChange={handleEvent}/>
+                    <label htmlFor='isFriendly'>Are you friendly?</label>
+
+                    
+                    <fieldset>
+                        <legend>What is your current employment?</legend>
+
+                        {/* RADIO BUTTONS to set Controlled Component. We use a CHECKED attribute and set it as a Condition between State Variable and input Value  */}
+                        <input type="radio" name="employment" id="unemployed" value="unemployed" onChange={handleEvent} checked={formData.employment === "unemployed"}/>
+                        <label htmlFor='unemployed'>Unemployed</label>
+                        <br />
+                        <input type="radio" name="employment" id="full-time" value="full-time" onChange={handleEvent} checked={formData.employment === "full-time"}/>
+                        <label htmlFor='full-time'>Full Time</label>
+                        <br />
+                        <input type="radio" name="employment" id="part-time" value="part-time" onChange={handleEvent} checked={formData.employment === "part-time"}/>
+                        <label htmlFor='part-time'>Part Time</label>
+                        <br />
+                    </fieldset>
+
+                    <br />
+                    <label htmlFor="favColor">What is your favorite color?</label>
+                    <br />
+                    <select id="favColor" value={formData.favColor} name="favColor" onChange={handleEvent}>
+                        <option value="">Choose Color</option>
+                        <option value="red">Red</option>
+                        <option value="blue">Blue</option>
+                        <option value="green">Green</option>
+                        <option value="orange">Orange</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="indigo">Indigo</option>
+                        <option value="violet">Violet</option>
+                    </select>
+                    <br />
+                    <br />
+                    {/* If a button element is found inside a form tag. It automatically is seen as a submit. */}
+                    <button>Submit</button>
                 </form>
             </div>
         </div>
